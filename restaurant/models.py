56d3dc649d373048ab5099793fc74f773d0910a0
engrabdulmalik <engrabdulmaliq@gmail.com>
2023-06-28 07:37:51 +0500

    Registering Models

--- restaurant/models.py
+++ restaurant/models.py
@@ -2,12 +2,20 @@ from django.db import models
 
 # Create your models here
 
+
 class Menu(models.Model):
     title = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    inventory = models.IntegerField(max_length=5)
-    
+    inventory = models.IntegerField()
+
+    def __str__(self) -> str:
+        return self.title
+
+
 class Booking(models.Model):
     name = models.CharField(max_length=100)
-    no_of_guests = models.IntegerField(max_length=6)
-    booking_date = models.DateTimeField(auto_now=True)
+    no_of_guests = models.IntegerField()
+    booking_date = models.DateField(db_index=True)
+
+    def __str__(self) -> str:
+        return self.name
